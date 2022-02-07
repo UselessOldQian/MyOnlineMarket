ef7c9f5244a4617a424e13e72c4dbae2e2c75e38
craigqian <qian.xingj@northeastern.edu>
2022-02-06 23:38:48 -0800

    init

---
+++ mymall-ware/src/test/java/com/craig/ware/mymallware/MymallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craig.ware.mymallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallWareApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
